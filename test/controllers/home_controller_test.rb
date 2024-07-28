6899237395c078db174dfd68004956961b3eef11
David Trussler <mail@davidtrussler.net>
2024-07-28 01:21:36 +0100

    Add dynamic `<title>` element to `application.html.erb`

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -4,5 +4,6 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do
     get home_show_url
     assert_response :success
+    assert_select "title", "Home | Thin Man Reviews"
   end
 end
--- test/controllers/review_controller_test.rb
+++ test/controllers/review_controller_test.rb
@@ -4,5 +4,6 @@ class ReviewControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do
     get review_show_url
     assert_response :success
+    assert_select "title", "Review | Thin Man Reviews"
   end
 end
