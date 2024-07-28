1c4acf1ce2f782fae6fd6f231540015417070216
David Trussler <mail@davidtrussler.net>
2024-07-28 15:54:31 +0100

    Add root route

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get home_show_url
+  test "should get home page" do
+    get root_url
     assert_response :success
     assert_select "title", "Home | Thin Man Reviews"
   end
--- test/controllers/review_controller_test.rb
+++ test/controllers/review_controller_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class ReviewControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
+  test "should get review page" do
     get review_show_url
     assert_response :success
     assert_select "title", "Review | Thin Man Reviews"
