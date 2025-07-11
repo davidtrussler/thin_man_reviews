1589f50448853de4b9c1bdbe5232449b62d22006
davidtrussler <mail@davidtrussler.net>
2025-07-11 16:53:36 +0100

    Update homepage_test to test list of reviews

--- test/integration/homepage_test.rb
+++ test/integration/homepage_test.rb
@@ -1,8 +1,27 @@
 require "test_helper"
 
 class HomepageTest < ActionDispatch::IntegrationTest
-  # Stub some reviews
-  # test that they are rendering as expected
+  test "the page lists details of all saved reviews" do
+    # Fetch reviews from fixtures
+    # test that they are rendering as expected
+    # For some reason they seem to be fetched in reverse order
+    visit_homepage
+
+    mediums = ["Recorded music", "Cinema", "Theatre"]
+    titles = ["Open the Gates", "The Souvenir", "The Collaboration"]
+    extras = ["By Irreversible Entanglements", "Directed by Joanna Hogg", "By Anthony McCarten"]
+
+    css_select(".reviews-listing").each_with_index do |listing, index|
+      assert_dom listing, "p.listing__medium", mediums[index]
+      assert_dom listing, "p.listing__title", titles[index]
+      assert_dom listing, "p.listing__extra", extras[index]
+    end
+  end
 
   # Test that the fallback image renders when there is not an image uploaded
+
+private
+  def visit_homepage
+    get root_path
+  end
 end
