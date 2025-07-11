1d259cda410b11057883ea3eefac847ae3b6e49d
davidtrussler <mail@davidtrussler.net>
2025-07-11 12:27:34 +0100

    Add integration test for homepage (WIP)

---
+++ test/integration/homepage_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class HomepageTest < ActionDispatch::IntegrationTest
+  # Stub some reviews
+  # test that they are rendering as expected
+
+  # Test that the fallback image renders when there is not an image uploaded
+end
