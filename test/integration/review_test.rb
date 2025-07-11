bbc362f1ee719f02f2aec46c1972b0126ed4f943
davidtrussler <mail@davidtrussler.net>
2025-07-11 12:28:45 +0100

    Add integration test for review page (WIP)

---
+++ test/integration/review_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class ReviewTest < ActionDispatch::IntegrationTest
+  # Stub a review
+  # test that it is rendering as expected
+
+  # Test that the fallback image renders when there is not an image uploaded
+end
