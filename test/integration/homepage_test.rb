b5c3ee2f27780c6c6e455376e3eea766010ab6fa
David Trussler <mail@davidtrussler.net>
2025-07-12 20:22:51 +0100

    Stub a test for clicking on each review - Need to solve how to find the link to click

--- test/integration/homepage_test.rb
+++ test/integration/homepage_test.rb
@@ -1,21 +1,24 @@
 require "test_helper"
 
 class HomepageTest < ActionDispatch::IntegrationTest
-  test "the page lists details of all saved reviews" do
-    # Fetch reviews from fixtures
-    # test that they are rendering as expected
-    # For some reason they seem to be fetched in reverse order
+  setup do
+    @mediums = ["Recorded music", "Cinema", "Theatre"]
+    @titles = ["Open the Gates", "The Souvenir", "The Collaboration"]
+    @extras = ["By Irreversible Entanglements", "Directed by Joanna Hogg", "By Anthony McCarten"]
+
     visit_homepage
+  end
 
-    mediums = ["Recorded music", "Cinema", "Theatre"]
-    titles = ["Open the Gates", "The Souvenir", "The Collaboration"]
-    extras = ["By Irreversible Entanglements", "Directed by Joanna Hogg", "By Anthony McCarten"]
-
+  test "the page lists details of all saved reviews on load" do
     css_select(".reviews-listing").each_with_index do |listing, index|
-      assert_dom listing, "p.listing__medium", mediums[index]
-      assert_dom listing, "p.listing__title", titles[index]
-      assert_dom listing, "p.listing__extra", extras[index]
+      assert_dom listing, "p.listing__medium", @mediums[index]
+      assert_dom listing, "p.listing__title", @titles[index]
+      assert_dom listing, "p.listing__extra", @extras[index]
     end
+  end
+
+  test "clicking on each review link navigates to the review page for that review" do
+    skip("Can't find the link to click")
   end
 
   # Test that the fallback image renders when there is not an image uploaded
