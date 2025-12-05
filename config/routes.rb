8e5f6eea2f2a23d92e87a6bcfe5274fe984a9d57
David Trussler <mail@davidtrussler.net>
2025-12-05 16:36:19 +0000

    Add login logic for admin area (#9)
    - Create User controller
      - creates controller, helper, view (new), controller test
      - updates routes
    - Create User model
      - creates model, db migration, model test and fixtures
    - Add validation and passwords to users
    - Create user session
      - update routes
    - Update generated sessions controller test for updated routes
    - Create login view
    - Add sessions controller
      - add methods: new/create/destroy
    - Add flash message if user not found or invalid
    - Add users login integration test
    - Add sessions_helper module to define a login method
    - Add the sessions_helper to the base controller to make is available everywhere
    - Update controller to provide logic for succesful login

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,25 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get '/review', to: 'review#show'
+  # get 'sessions/new'
+  # get 'users/new'
 
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   root "home#show"
+
+  get '/review', to: 'review#show'
 
   resources :review
 
   # Path to admin landing page
   get '/admin', to: 'admin#show'
 
+  # Add a new review
   get '/admin/new', to: 'admin#new'
   post '/admin/new', to: 'admin#create'
+
+  # Admin login page
+  # Creates/gets/destroys user sessions
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 end
---
+++ test/models/user_test.rb
@@ -0,0 +1,70 @@
+require "test_helper"
+
+class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = User.new(
+      name: "Alice User",
+      email: "alice@user.com",
+      password: "foobar",
+      password_confirmation: "foobar"
+    )
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "user name should be present" do
+    @user.name = "  "
+    assert_not @user.valid?
+  end
+
+  test "user email should be present" do
+    @user.email = "  "
+    assert_not @user.valid?
+  end
+
+  test "user name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "user email should not be too long" do
+    @user.email = "a" * 257 + "@user.com"
+    assert_not @user.valid?
+  end
+
+  test "email validation should accept valid email addresses" do
+    valid_addresses = %w[alice@user.com FOO@BAR.COM A_LI-CE@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "email validation should reject invalid email addresses" do
+    invalid_addresses = %w[alice@user,com alice_at_foo.org alice@baz alice@foo.bar_baz.com alice@foo.bar+baz.com]
+
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "user password should not be blank" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "user password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+end
