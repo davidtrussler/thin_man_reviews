06be1def46a4aeeee0608ad49d1a6a3e2661516a
David Trussler <david.trussler@digital.cabinet-office.gov.uk>
2026-07-03 17:24:53 +0100

    Update `admin_controller_test.rb` to add tests for legitimate users (WIP)

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 class AdminControllerTest < ActionDispatch::IntegrationTest
-  test "should get the Admin landing page" do
+  # def setup
+  #   @user = users(:david)
+  # end
+
+  test "should get the Admin landing page if the current user has permissions" do
+    @current_user = users(:david)
+
+    puts "++@current_user++"
+    puts @current_user
+    puts "++++"
+
+    get admin_path
+    assert true
+  end
+
+  test "should return the user to the login page if they do not have permissions" do
+    @current_user = nil
+
+    puts "++@current_user++"
+    puts @current_user
+    puts "++++"
+
     get admin_path
     assert true
   end
