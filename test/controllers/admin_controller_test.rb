79fdf2c7102af6bffa3373aa6e12a85389542679
David Trussler <david.trussler@digital.cabinet-office.gov.uk>
2026-07-03 15:47:45 +0100

    Update `admin_controller_test.rb` to add test for "should get the Admin landing page"

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class AdminControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get the Admin landing page" do
+    get admin_path
+    assert true
+  end
 end
