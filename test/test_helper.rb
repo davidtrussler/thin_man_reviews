56d81f2438321a93d3e8d7c735664f07aced7fe9
David Trussler <david.trussler@digital.cabinet-office.gov.uk>
2026-08-29 12:27:38 +0100

    Add `log_in_as` method to helper - This is not used anywhere at the minute - part of trying to solve the test for authorised users

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+require "sessions_helper"
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
@@ -9,5 +10,7 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
 end
