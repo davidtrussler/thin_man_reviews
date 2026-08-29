59a8b940a0662ceab4e133104f97a58771ea4876
David Trussler <david.trussler@digital.cabinet-office.gov.uk>
2026-08-29 12:24:16 +0100

    Update `admin_controller_test` to add test "should render the Admin landing page if the current user has permissions" (WIP) - I have just not been able to get this to work so might skip it for now and come back to it.

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -1,14 +1,6 @@
 require "test_helper"
-# require "sessions_helper"
-# require "../admin_controller"
 
 class AdminControllerTest < ActionDispatch::IntegrationTest
-  # def setup
-    # reset_session
-    # log_in(:david)
-  #   @user = users(:david)
-  # end
-
   test "should return the user to the login page if they do not have permissions with message" do
     get admin_path
 
@@ -17,79 +9,12 @@ class AdminControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should render the Admin landing page if the current user has permissions" do
-    user = users(:david) # User.create(email: "mail@davidtrussler.net")
+    @user = users(:david) # User.create(email: "mail@davidtrussler.net")
 
-    puts "++user++"
-    puts user
-    puts "++++"
+    reset_session
 
-    # sign_in_as(user)
-    # SessionsHelper::log_in(user)
+    get admin_path
 
-    # require_permissions = true
-    # @current_user = user
-
-    # reset_session
-    # log_in user    # @current_user = users(:david)
-    # sign_in_as users(:david)
-    # SessionsHelper::log_in(:david)
-
-    # puts "++@current_user++"
-    # puts @current_user
-    # puts "++++"
-
-    get admin_path # , params: { user_id: user.id }
-
-    # session[:user_id] = user.id
-
-    # puts "++current_user++"
-    # puts @current_user
-    # puts "++++"
-
-    # puts "++controller++"
-    # puts @controller.action_name
-    # puts "++++"
-
-    # puts "++request++"
-    # puts @request
-    # puts "++++"
-
-    # puts "++response++"
-    # puts @response.body
-    # puts "++++"
-
-    # puts "++session++"
-    # puts session[:user_id]
-    # puts "++++"
-
-    # get :show
-    # assert true
-    # assert_template "admin/show"
     assert_response :success
   end
-
-  def sign_in_as(user)
-    # post sign_in_url(email: user.email, password: user.password)
-    # log_in(user)
-    puts "++user++"
-    puts user
-    puts "++++"
-
-    log_in(user)
-
-    # post login_path, params: {
-    #   session: {
-    #     email: user.email,
-    #     password: user.password
-    #   }
-    # }
-  end
-
-  # def login_as(user)
-  #   request.env["warden"] = stub(authenticate!: true, authenticated?: true, user:)
-  # end
-
-  # def log_in_as(user)
-  #   log_in(user)
-  # end
 end
